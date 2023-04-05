c54bc8f87086c073ede8a7a53d747491225bc9c3
Edwin Saul <edwinsaulpm@gmail.com>
2023-04-04 19:40:13 -0500

    pages example

---
+++ proyectos/p01_holaMundo/README.md
@@ -0,0 +1,7 @@
+# Hola mundo
+Mas información en 
+[Aquí](https://saul11235.github.io/Experimentos_arduino/#HolaMundo)
+
+<p align="center">
+  <img src="grafico.svg" alt="imagen" />
+</p>
--- proyectos/p01_holaMundo/p01_holaMundo.ino
+++ proyectos/p01_holaMundo/p01_holaMundo.ino
@@ -1,9 +1,9 @@
 /* Esto es un comentario no se ejecuta
-puede hacerse en varias lineas */
-
-// o tambien solo en una linea
-
-/* ----------------------------
+puede hacerse en varias lineas 
+ ----------------------------
+Tambien puede comenzar con //
+epro solo sera para una linea
+ -----------------------------
 En Arduino hay dos partes principales 
 la primera es setup() en donde se configura el entorno
 y la segunda es loop() que es un loop (teoricamente) infinito
@@ -19,8 +19,10 @@ void es una funcion que no devuelde un resultado
 
 
 void setup() {
- //  configuramos serial a 9600Bd baudios
+ /*  configuramos serial a 9600Bd baudios */
   Serial.begin(9600);
+  // coloca un string en el serial
+  Serial.println("Hola mundo"); 
  /*  -------------------------------
  Nota.- un baudio es una unidad que expresa 
         velocidad de transmision de informacion
@@ -30,9 +32,6 @@ void setup() {
 
 
 void loop() {
-  // coloca un string en el serial
-  Serial.println("Hola mundo!"); 
-  // espera 1000 milisegundos (1s)
+  /*Esto puede estar en blanco*/
   delay(1000);
 }
-
